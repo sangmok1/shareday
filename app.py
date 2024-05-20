568ab21ee5a83059f1b506a5b7b00148604ed5ec
sm-lee <sm.lee@krafton.com>
2024-05-20 20:27:19 +0900

    [META-0] Upload file

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template, redirect, url_for
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB Max
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = file.filename
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            return redirect(url_for('upload_file'))
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
